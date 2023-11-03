8944f3278587dd5c4a443e08958ce839cd3e358f
kleos <36254493+amlitio@users.noreply.github.com>
2023-11-03 14:47:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const API_BASE_URL = 'https://statsapi.web.nhl.com/api/v1';
 
 const teamSelect = document.getElementById('team-select');
 const statsContainer = document.getElementById('stats-container');
@@ -6,60 +6,66 @@ const loadingIndicator = document.getElementById('loading-indicator');
 
 // Function to display loading indicator
 function showLoadingIndicator() {
-    loadingIndicator.style.display = 'block';
-    statsContainer.style.display = 'none';
+  loadingIndicator.style.display = 'block';
+  statsContainer.style.display = 'none';
 }
 
 // Function to hide loading indicator
 function hideLoadingIndicator() {
-    loadingIndicator.style.display = 'none';
-    statsContainer.style.display = 'block';
+  loadingIndicator.style.display = 'none';
+  statsContainer.style.display = 'block';
 }
 
-// Fetch NHL teams from the API and populate the dropdown
-axios.get('https://statsapi.web.nhl.com/api/v1/teams')
-    .then(response => {
-        const teams = response.data.teams;
-        teams.forEach(team => {
-            const option = document.createElement('option');
-            option.value = team.id;
-            option.textContent = team.name;
-            teamSelect.appendChild(option);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching teams:', error);
-        teamSelect.disabled = true; // Disable the dropdown on error
+// Function to populate the dropdown with teams from the API
+async function populateDropdown() {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/teams`);
+    const teams = response.data.teams;
+
+    teams.forEach((team) => {
+      const option = document.createElement('option');
+      option.value = team.id;
+      option.textContent = team.name;
+      teamSelect.appendChild(option);
     });
+  } catch (error) {
+    console.error('Error fetching teams:', error);
+    teamSelect.disabled = true; // Disable the dropdown on error
+  }
+}
 
-// Event listener for when a team is selected
-teamSelect.addEventListener('change', () => {
+// Function to fetch the roster for the selected team from the API
+async function fetchTeamRoster() {
+  const selectedTeamId = teamSelect.value;
+
+  try {
     showLoadingIndicator();
+    const response = await axios.get(`${API_BASE_URL}/teams/${selectedTeamId}/roster`);
+    const roster = response.data.roster;
 
-    const selectedTeamId = teamSelect.value;
+    // Clear the statsContainer
+    statsContainer.innerHTML = '';
 
-    // Fetch stats for the selected team from the API
-    axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${selectedTeamId}/roster`)
-        .then(response => {
-            hideLoadingIndicator();
-            const roster = response.data.roster;
+    if (roster.length > 0) {
+      // Display player stats
+      statsContainer.innerHTML = `<h2>Roster for selected team:</h2>`;
+      roster.forEach((player) => {
+        statsContainer.innerHTML += `<p>${player.person.fullName} - ${player.position.name}</p>`;
+      });
+    } else {
+      statsContainer.innerHTML = '<p>No players found for the selected team.</p>';
+    }
 
-            // Clear the statsContainer
-            statsContainer.innerHTML = '';
+    hideLoadingIndicator();
+  } catch (error) {
+    hideLoadingIndicator();
+    console.error('Error fetching team roster:', error);
+    statsContainer.innerHTML = '<p>An error occurred while fetching player stats.</p>';
+  }
+}
 
-            if (roster.length > 0) {
-                // Display player stats
-                statsContainer.innerHTML = `<h2>Roster for selected team:</h2>`;
-                roster.forEach(player => {
-                    statsContainer.innerHTML += `<p>${player.person.fullName} - ${player.position.name}</p>`;
-                });
-            } else {
-                statsContainer.innerHTML = '<p>No players found for the selected team.</p>';
-            }
-        })
-        .catch(error => {
-            hideLoadingIndicator();
-            console.error('Error fetching team roster:', error);
-            statsContainer.innerHTML = '<p>An error occurred while fetching player stats.</p>';
-        });
-});
+// Add event listener for when a team is selected
+teamSelect.addEventListener('change', fetchTeamRoster);
+
+// Populate the dropdown with teams from the API
+populateDropdown();
